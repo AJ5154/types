64e5252df1dc323ddee8e31c0474fd7eab1f8afd
AJ5154 <ashishjadhav597@gmail.com>
2024-01-04 15:25:11 +0530

    Add litrals to role property in UserRole

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 type PrefixName = "MR" | "MRS" | "MISS";
 
+type Role = "USER" | "SUPERADMIN" | "ADMIN";
+
 type UserRole = {
-  id: "54773786-8a1e-46d7-9a3c-15ab268ab035";
-  role: "USER";
-  userId: "62b585e4-f7dd-49db-b9b6-51293dadad1c";
-  createdAt: "2023-11-26T20:16:43.474Z";
+  id: string;
+  role: Role;
+  userId: string;
+  createdAt: string;
 };
 
 type User = {
