e61d2ecd6324db228f92fcbc9c10fc786b89057e
AJ5154 <ashishjadhav597@gmail.com>
2024-01-04 15:21:02 +0530

    Add literal type to prefix property in user

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
+type PrefixName = "MR" | "MRS" | "MISS";
+
 type User = {
-    id: "62b585e4-f7dd-49db-b9b6-51293dadad1c";
-    prefix: "MR";
-    firstName: "string";
-    lastName: "string";
-    phone: "1234567890";
-    email: "user@yopmail.com";
+    id: string;
+    prefix: PrefixName;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    email: string;
     userRoles: [
         {
             id: "54773786-8a1e-46d7-9a3c-15ab268ab035";
@@ -16,7 +18,7 @@ type User = {
 };
 
 type Token = {
-    accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjU4NWU0LWY3ZGQtNDlkYi1iOWI2LTUxMjkzZGFkYWQxYyIsInByZWZpeCI6Ik1SIiwiZmlyc3ROYW1lIjoic3RyaW5nIiwibGFzdE5hbWUiOiJzdHJpbmciLCJwaG9uZSI6IjEyMzQ1Njc4OTAiLCJlbWFpbCI6InVzZXJAeW9wbWFpbC5jb20iLCJ1c2VyUm9sZXMiOlt7ImlkIjoiNTQ3NzM3ODYtOGExZS00NmQ3LTlhM2MtMTVhYjI2OGFiMDM1Iiwicm9sZSI6IlVTRVIiLCJ1c2VySWQiOiI2MmI1ODVlNC1mN2RkLTQ5ZGItYjliNi01MTI5M2RhZGFkMWMiLCJjcmVhdGVkQXQiOiIyMDIzLTExLTI2VDIwOjE2OjQzLjQ3NFoifV0sImlhdCI6MTcwNDM1MzE4MCwiZXhwIjoxNzA0OTU3OTgwfQ.l9AjdLRbyYFokFtgcrUrThswJ1Kte8IMYMz6pYWzKdI";
+    accessToken: string;
 };
 
 type Entity = {
