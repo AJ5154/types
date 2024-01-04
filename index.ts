3bcc3f3d13e144712b3bc33b8189fcf0a4a50993
AJ5154 <ashishjadhav597@gmail.com>
2024-01-04 15:22:48 +0530

    Extract the UserRole array

--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
 type PrefixName = "MR" | "MRS" | "MISS";
 
+type UserRole = {
+  id: "54773786-8a1e-46d7-9a3c-15ab268ab035";
+  role: "USER";
+  userId: "62b585e4-f7dd-49db-b9b6-51293dadad1c";
+  createdAt: "2023-11-26T20:16:43.474Z";
+};
+
 type User = {
-    id: string;
-    prefix: PrefixName;
-    firstName: string;
-    lastName: string;
-    phone: string;
-    email: string;
-    userRoles: [
-        {
-            id: "54773786-8a1e-46d7-9a3c-15ab268ab035";
-            role: "USER";
-            userId: "62b585e4-f7dd-49db-b9b6-51293dadad1c";
-            createdAt: "2023-11-26T20:16:43.474Z";
-        }
-    ];
+  id: string;
+  prefix: PrefixName;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  userRoles: UserRole[];
 };
 
 type Token = {
-    accessToken: string;
+  accessToken: string;
 };
 
 type Entity = {
-    user: User;
-    token: Token;
+  user: User;
+  token: Token;
 };
 
 type Data = {
